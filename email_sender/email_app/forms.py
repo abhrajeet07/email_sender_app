9b97a757a407915911bb1526e067cd905c415620
abhrajeet07 <78360509+abhrajeet07@users.noreply.github.com>
2024-07-24 02:36:14 +0530

    Delete email_sender directory

--- email_sender/email_app/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class EmailForm(forms.Form):
-    email_file = forms.FileField()
-    subject = forms.CharField(max_length=255)
-    body = forms.CharField(widget=forms.Textarea)
--- email_sender/email_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import send_email  # Update this line
-
-urlpatterns = [
-    path('', send_email, name='send_email'),  # Ensure this path is correctly named
-]
